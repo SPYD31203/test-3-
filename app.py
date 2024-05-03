19f023645577d5a2fd4ee952f6fd633af2477d74
SPYD31203 <ankitchawla31203@gmail.com>
2024-05-03 13:54:30 +0530

    new

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, jsonify
+from runprediction import predictionsz
+
+app = Flask(__name__)
+
+@app.route('/api', methods=['POST'])
+def api():
+    data = request.json
+    content = data.get('content')
+    recommended_asans = predictionsz(content)
+    print("Received content:", content)
+    return jsonify({"recommended_asans": recommended_asans})
+
+if __name__ == '__main__':
+    app.run(debug=True)
